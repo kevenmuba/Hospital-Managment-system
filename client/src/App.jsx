154b894555837022bb1b8a5cd0eec9679cb49149
kevenmuba <kevk0758@gmail.com>
2024-09-22 18:18:13 +0300

    finish frontend part of authentication and header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+// import Header from './Components/Header/Header'
+import Layout from './LayOut'
+ import IndexPage from './Pages/indexPage'
+import LoginPage from './Pages/LoginPage'
+import RegisterPage from './Pages/RegisterPage'
 
 function App() {
  
 
   return (
     <>
+    <Routes >
+    <Route path='/' element={<Layout/>} > 
+    <Route path="/home" element={<IndexPage/>}/>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage/>} />
+    </Route>
+    
+
+    </Routes>
       
     </>
   )
